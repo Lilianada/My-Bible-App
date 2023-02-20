b4e8cde21be0baea7527c41483e52b469f40618c
Lilianada <lilibest30.lb@gmail.com>
2023-02-20 06:07:09 -0500

    ...Bible Api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+// import useFetch from './services/useFetch';
+import axios from "axios";
+
 
 function App() {
+  const useFetch = {
+    method: 'GET',
+    url: 'https://ajith-holy-bible.p.rapidapi.com/GetBooks',
+    headers: {
+      'X-RapidAPI-Key': '0e0b03802dmsh0ff85f33cd9c03cp167843jsne657c6b58de7',
+      'X-RapidAPI-Host': 'ajith-holy-bible.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(useFetch).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
